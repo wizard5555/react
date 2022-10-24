212403436a537b8b16ce845b719e7e5b016d1710
Daniel Patricio Lima <patricio.wizard@gmail.com>
2022-10-24 19:02:03 -0300

    Projeto inicial

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import {Button, TextField} from "@mui/material";
+import {useState} from "react";
+import "./styles.css";
+
+export default function App () {
+  const [cep, setCep] = useState('');
+  const [logradouro, setLogradouro] = useState('');
+  const [bairro, setBairro] = useState('');
+
+  const buscarEndereco = () => {
+      fetch(`http://viacep.com.br/ws/${cep}/json`)
+        .then(res => res.json())
+        .then(dados => {
+          setBairro(dados.bairro);
+          setLogradouro(dados.logradouro);
+        });
+  };
+
+  return (
+    <div className="endereco">
+      <form>
+        <TextField onBlur={buscarEndereco} onChange={event => setCep(event.target.value)} value={cep} label="CEP" fullWidth/>
+        
+        <TextField value={logradouro} onChange={event => setLogradouro(event.target.value)} label="Logradouro" fullWidth/>
+
+        <TextField value={bairro} onChange={event => setBairro(event.target.value)} label="Bairro" fullWidth/>
+      </form>
+    </div>
+  )
+}
